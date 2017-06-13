f2dab1f9436a55e3166ec28c30e41218b6bf6797
Clay <wcl58981298@gmail.com>
2017-06-13 09:54:04 +0800

    1.5.0 | add branch page

---
+++ weapp/pages/branch/branch.js
@@ -0,0 +1,61 @@
+const {
+  request,
+} = require('../../common/js/promise_api.js');
+
+Page({
+  data: {
+    query: {
+    },
+    errText: '',
+    branchs: [],
+  },
+  showError(err) {
+    const setdata = {
+      errText: String(JSON.stringify(err))
+    };
+    this.setData(setdata);
+    wx.hideLoading();
+  },
+  onPullDownRefresh() {
+    this.getBranchs(() => {
+      wx.stopPullDownRefresh();
+    });
+  },
+  getBranchs: function (callback) {
+    const self  = this;
+    // 每次请求提示加载中
+    wx.showLoading({
+      title: 'loading...'
+    });
+    request({
+      url: 'https://www.unclay.com/cache',
+      data: {
+        url: `https://api.github.com/repos/${self.data.query.user}/${self.data.query.name}/branches`,
+        expire: 60 * 60
+      }
+    }).then((res) => {
+      if (res.data.status && res.data.response) {
+        return self.showError(res.data.response.text);
+      }
+      self.setData({
+        branchs: res.data,
+      });
+      wx.hideLoading();
+      callback && callback(null);
+    }).catch((err) => {
+      self.showError(err && err.errMsg);
+      callback && callback(err);
+    });
+  },
+  onLoad({ name, user }) {
+    this.data.query.name = name;
+    this.data.query.user = user;
+    this.setData({
+      query: this.data.query,
+    });
+    this.getBranchs();
+    wx.setNavigationBarTitle({
+      title: 'Branchs'
+    });
+  }
+})
